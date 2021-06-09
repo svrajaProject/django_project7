f604e4c584593a928afa0d48dc0f150429b0f120
svrajaProject <softwareengineerraja@gmail.com>
2021-06-10 00:27:38 +0530

    Post is not iterable bug fixed

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from blog.models import Post
+from .models import Post
 # posts = [
 #     {
 #         'author': 'Raja',
@@ -18,7 +18,7 @@ from blog.models import Post
 
 def home(request):
     context = {
-        'posts' : Post
+        'posts' : Post.objects.all()
     }
     return render(request,'blog/home.html', context)
 def about(request):
