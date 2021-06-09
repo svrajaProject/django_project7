cfbc30787384db7f537b990a629c062de0d0393e
svrajaProject <softwareengineerraja@gmail.com>
2021-06-10 00:03:33 +0530

    Dynamic data

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Raja',
-        'title':'Ponniyin Selvan',
-        'content': 'First content',
-        'date_posted':'May 23  2021'
-    },
-    {
-        'author': 'Tinku',
-        'title':'AAthichudi',
-        'content': 'Second content',
-        'date_posted':'May 24 2021'
-    }
-]
+from blog.models import Post
+# posts = [
+#     {
+#         'author': 'Raja',
+#         'title':'Ponniyin Selvan',
+#         'content': 'First content',
+#         'date_posted':'May 23  2021'
+#     },
+#     {
+#         'author': 'Tinku',
+#         'title':'AAthichudi',
+#         'content': 'Second content',
+#         'date_posted':'May 24 2021'
+#     }
+# ]
 
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post
     }
     return render(request,'blog/home.html', context)
 def about(request):
--- django_project7/settings.py
+++ django_project7/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'django_project7.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PASSWORD': 'test@123',
+        'HOST':'localhost'
     }
 }
 
