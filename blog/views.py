d15a5a07652b0bd03610dbb242ffad9c579df4ef
svrajaProject <softwareengineerraja@gmail.com>
2021-07-16 17:37:42 +0530

    Output image size reduced

--- blog/views.py
+++ blog/views.py
@@ -21,5 +21,6 @@ def home(request):
         'posts' : Post.objects.all()
     }
     return render(request,'blog/home.html', context)
+
 def about(request):
     return render(request,'blog/about.html', {'title':'About'})
